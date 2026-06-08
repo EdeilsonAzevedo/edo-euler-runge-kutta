3e7aeabef2c88db3f3a694a4b9f94822dc9e3def
vinicius <vcn0510@gmail.com>
2026-06-08 15:53:56 -0300

    feat: adiciona Método de Taylor e Método de Euler Melhorado

--- src/euler.py
+++ src/euler.py
@@ -1,11 +1,14 @@
 import numpy as np
-from utils import plot_compare, plot_function
+from utils import plot_compare4
 
 def f(t, y):
-	return np.cos(3*t) * np.exp(-t) - y
+    return y + 2 * np.exp(t) * np.cos(t)
+
+def df(t, y):
+    return f(t, y) + 2 * np.exp(t) * (np.cos(t) - np.sin(t))
 
 def y(t):
-    return (np.exp(-t) * np.sin(3*t)) / 3
+    return np.exp(t) * (2 * np.sin(t) + 1)
 
 def euler(f, y0, a, b, N):
     h = (b - a) / N
@@ -18,16 +21,40 @@ def euler(f, y0, a, b, N):
         
     return t, w
 
-if __name__ == "__main__":
-    y0 = 0
-    a = 0.0
-    b = 6.0
-    N = 50
+def taylor2(f, df, y0, a, b, N):
+    h = (b - a) / N
+    t = np.linspace(a, b, N + 1)
+    w = np.zeros(N + 1)
+    
+    w[0] = y0
+    for i in range(N):
+        w[i+1] = w[i] + h * f(t[i], w[i]) + (h**2 / 2) * df(t[i], w[i])
+        
+    return t, w
 
-    t_i, w = euler(f, y0, a, b, N)
+def euler_melhorado(f, y0, a, b, N):
+    h = (b - a) / N
+    t = np.linspace(a, b, N + 1)
+    w = np.zeros(N + 1)
+    
+    w[0] = y0
+    for i in range(N):
+        w[i+1] = w[i] + (h / 2.0) * (f(t[i], w[i]) + f(t[i+1], w[i] + h * f(t[i], w[i])))
+        
+    return t, w
+
+if __name__ == "__main__":
+    y0 = 1
+    a = 0.0
+    b = 2.5
+    N = 12
+
+    t_euler, w_euler = euler(f, y0, a, b, N)
+    t_taylor, w_taylor = taylor2(f, df, y0, a, b, N)
+    t_melhorado, w_melhorado = euler_melhorado(f, y0, a, b, N)
     # plot_function(t_i, w)
 
-	# Comparar a solução aproximada com a solução exata
+	# plotar soluções aproximadas vs a solução exata
     t = np.linspace(a, b, 256)
-    plot_compare(t, y(t), t_i, w, (b - a) / N, name="Método de Euler")
+    plot_compare4(t, y(t), t_euler, w_euler, t_taylor, w_taylor, t_melhorado, w_melhorado, first_name="Solução Exata", second_name="Método de Euler", third_name="Método de Taylor 2ª Ordem", fourth_name="Método de Euler Melhorado")
     
--- src/utils.py
+++ src/utils.py
@@ -13,15 +13,46 @@ def plot_function(t, y):
 
     plt.show()
 
-def plot_compare(t, y, t_i, w, h, name="Método de Euler"):
+def plot_compare2(t_1, y_1, t_2, y_2, first_name="", second_name=""):
     plt.figure(figsize=(10, 6))
 
-    plt.plot(t, y, label='Solução Exata', color='blue', linewidth=2)
+    plt.plot(t_1, y_1, label=first_name, color='blue', linewidth=2)
 
-    plt.plot(t_i, w, label=f'{name} (h = {h})', 
+    plt.plot(t_2, y_2, label=second_name, 
              color='red', linestyle='--', marker='o', linewidth=1, markersize=5)
 
-    plt.title(f'Solução Exata vs {name}', fontsize=14)
+    plt.title(f'{first_name} vs {second_name}', fontsize=14)
+    plt.xlabel('t', fontsize=12)
+    plt.ylabel('y', fontsize=12)
+    plt.grid(True, linestyle=':', alpha=0.7)
+    plt.legend(fontsize=12)
+
+    plt.show()
+
+def plot_compare3(t_1, y_1, t_2, y_2, t_3, y_3, first_name="", second_name="", third_name=""):
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(t_1, y_1, label=first_name, color='blue', linewidth=2)
+    plt.plot(t_2, y_2, label=second_name, color='green', linestyle='--', marker='o', linewidth=1, markersize=5)
+    plt.plot(t_3, y_3, label=third_name, color='red', linestyle='--', marker='o', linewidth=1, markersize=5)
+
+    plt.title(f'{first_name} vs {second_name} vs {third_name}', fontsize=14)
+    plt.xlabel('t', fontsize=12)
+    plt.ylabel('y', fontsize=12)
+    plt.grid(True, linestyle=':', alpha=0.7)
+    plt.legend(fontsize=12)
+
+    plt.show()
+
+def plot_compare4(t_1, y_1, t_2, y_2, t_3, y_3, t_4, y_4, first_name="", second_name="", third_name="", fourth_name=""):
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(t_1, y_1, label=first_name, color='blue', linewidth=2)
+    plt.plot(t_2, y_2, label=second_name, color='green', linestyle='--', marker='o', linewidth=1, markersize=5)
+    plt.plot(t_3, y_3, label=third_name, color='red', linestyle='--', marker='o', linewidth=1, markersize=5)
+    plt.plot(t_4, y_4, label=fourth_name, color='orange', linestyle='--', marker='o', linewidth=1, markersize=5)
+
+    plt.title(f'{first_name} vs {second_name} vs {third_name} vs {fourth_name}', fontsize=14)
     plt.xlabel('t', fontsize=12)
     plt.ylabel('y', fontsize=12)
     plt.grid(True, linestyle=':', alpha=0.7)
